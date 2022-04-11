04af16729866d84cc96b1a52af1dfac19e09a8c9
João Silva <49305903+Joaomcns@users.noreply.github.com>
2022-04-11 23:06:18 +0100

    Add files via upload

---
+++ app.py
@@ -0,0 +1,14 @@
+import dash
+import dash_core_components as dcc
+import dash_html_components as html
+from dash.dependencies import Input, Output, State
+import dash_daq as daq
+import dash_bootstrap_components as dbc
+import numpy as np
+import pandas as pd
+import plotly.graph_objs as go
+import plotly.express as px
+
+
+
+path = 'https://raw.githubusercontent.com/InesRoque3/GroupV_project2/main/data/'
